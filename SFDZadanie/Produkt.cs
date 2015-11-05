d9f7793d5b52786e7749d72bcea3ed52e2d573bb
Basia-N <basia.najdziuk@sfd.pl>
2015-11-05 08:46:39 +0100

    1

---
+++ SFDZadanie/Produkt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFDZadanie
+{
+    public class Produkt
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public bool Dostepny { get; set; }
+        public decimal Cena { get; set; }
+
+
+        public static Produkt[] Pobierz()
+        {
+            return new Produkt[] {
+                new Produkt{ ID = 1, Nazwa="Glutamina", Dostepny=true, Cena=20.5m},
+                new Produkt{ ID = 2, Nazwa="Amino Plus", Dostepny=false, Cena=11.25m},
+                new Produkt{ ID = 3, Nazwa="Energy Musli", Dostepny=true, Cena=5.5m},
+                new Produkt{ ID = 4, Nazwa="Energy Drink", Dostepny=true, Cena=3.75m},
+                new Produkt{ ID = 5, Nazwa="Energy Baton", Dostepny=false, Cena=0.90m},
+                new Produkt{ ID = 6, Nazwa="Power Drink", Dostepny=false, Cena=1.90m}
+            };
+        }
+    }
+}
---
+++ SFDZadanie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFDZadanie
+{
+    /// <summary>
+    /// Program powinien w zależności od zadanego parametru wykonać odpowiednią funkcję
+    /// - "1": losuje produkt, który jest dostępny
+    /// - "2": wyświetla listę wszystkich dostępnych produktów posortowanych wg nazwy
+    /// - "3": wyświetla ilość wszystkich produktów
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+
+}
